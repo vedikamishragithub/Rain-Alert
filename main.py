f882d925856de97415c902119df795f8a7c1dee7
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2026-07-29 17:50:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
 import os
 import requests
 
-api_key = os.environ["OWM_API_KEY"]
+TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
 
-lat = 18.5204
-lon = 73.8567
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-url_weather = "https://api.openweathermap.org/data/2.5/forecast"
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ Test message from GitHub Actions"
+    }
+)
 
-params = {
-    "lat": lat,
-    "lon": lon,
-    "appid": api_key,
-    "cnt": 4
-}
-
-response = requests.get(url_weather, params=params)
-response.raise_for_status()
-weather_data = response.json()
-
-message = "✅ Hello from GitHub Actions!"
-
-for forecast in weather_data["list"]:
-    weather = forecast["weather"][0]["id"]
-
-    if 200 <= weather < 600:
-        message = "Bring an umbrella ☔-test"
-        break
-
-if message:
-    TOKEN = os.environ["BOT_TOKEN"]
-    CHAT_ID = os.environ["CHAT_ID"]
-
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    requests.post(
-        url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-    )
+print("Status Code:", response.status_code)
+print("Response:", response.text)
